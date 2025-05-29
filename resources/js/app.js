64ab8e1244edebb8317ce9a0955a20d1aafa0245
Mavi Joar C. Crisostomo <crisostomo.mavijoar2003@gmail.com>
2025-05-29 14:13:39 +0800

    install pinia and axios

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,10 @@
 import './bootstrap';
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
+import { createPinia} from 'pinia';
 import  '../css/app.css';
+
+const pinia = createPinia();
 
 createInertiaApp({
   resolve: name => {
@@ -11,6 +14,7 @@ createInertiaApp({
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
+      .use(pinia)
       .mount(el)
   },
 })
