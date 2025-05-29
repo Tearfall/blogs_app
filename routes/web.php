3d7623d3e6e43b5f6125902974bbd175fc08c89c
Mavi Joar C. Crisostomo <crisostomo.mavijoar2003@gmail.com>
2025-05-29 14:03:23 +0800

    install inertia and vue with tailwindcss

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\AuthController;
+
+Route::get('/', function (){
+    return inertia('Home');
+});
