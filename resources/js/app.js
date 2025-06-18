ea3d5fd404f24caea526b660de951184e2da7000
Mavi Joar C. Crisostomo <crisostomo.mavijoar2003@gmail.com>
2025-06-18 10:11:56 +0800

    Install font awesome icons

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,18 @@ import { createInertiaApp } from '@inertiajs/vue3'
 import { createPinia} from 'pinia';
 import  '../css/app.css';
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faPlus, faTimes, faEdit, faTrash, faHeart, faComment, faShare, faCheck } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faPlus, faTimes, faEdit, faTrash, faHeart, faComment, faShare, faCheck)
+
 const pinia = createPinia();
 
 createInertiaApp({
@@ -13,6 +25,7 @@ createInertiaApp({
   },
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
+      .component('font-awesome-icon', FontAwesomeIcon)
       .use(plugin)
       .use(pinia)
       .mount(el)
