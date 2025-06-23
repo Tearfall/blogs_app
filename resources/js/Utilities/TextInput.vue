82b935c2992a27cbd63887da99a5a5a6ce02e8b3
Mavi Joar C. Crisostomo <crisostomo.mavijoar2003@gmail.com>
2025-06-23 15:00:28 +0800

    Add reusable text input component

---
+++ resources/js/Utilities/TextInput.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="mb-6">
+        <label>{{ name }}</label>
+        <input :type="type" v-model="model" :class="{'!ring-red-500' : message}"/>
+        <small class="" v-if="message" >{{ message }}</small>
+
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        name:{
+            type: String,
+            required: true
+        },
+        type: {
+            type:String,
+            default: "text"
+        },
+        message: String,
+    },
+    setup(){
+        const model = defineModel({
+            type: null,
+            required: true
+        })
+
+        return model
+    }
+
+}
+</script>
